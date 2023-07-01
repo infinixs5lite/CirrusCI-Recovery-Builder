2720e93bf92dbf25f4d8d899a1382d36b4366838
RecoveryBuilder <ci.twrp@gmail.com>
2023-07-01 18:58:08 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
-export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/shazuxd/android_device_vivo_legend-twrp
-export DT_BRANCH=android-12.1
-export DEVICE_PATH=device/vivo/legend
-export DEVICE=legend
-export MAKEFILE=twrp_legend
-export TARGET=recoveryimage
-export OUTPUT=recovery.img
+export TWRP_BRANCH=twrp-11
+export DT_LINK=https://github.com/ShazuxD/extras
+export DT_BRANCH=android-11
+export DEVICE_PATH=device/xiaomi/fleur
+export DEVICE=fleur
+export MAKEFILE=twrp_fleur
+export TARGET=bootimage
+export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=30564
+export RANDOM=21679
