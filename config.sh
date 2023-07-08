070f96376d33e1677ed384de9ba8f9ffa0c5c2c4
RecoveryBuilder <ci.twrp@gmail.com>
2023-07-08 13:05:54 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-11
 export DT_LINK=https://github.com/ShazuxD/extras
-export DT_BRANCH=eleven
+export DT_BRANCH=android-11
 export DEVICE_PATH=device/xiaomi/fleur
 export DEVICE=fleur
 export MAKEFILE=twrp_fleur
 export TARGET=bootimage
 export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=17518
+export RANDOM=31137
