de8b1883413a5cef0b5d25c78c0582134ef1d58e
RecoveryBuilder <ci.twrp@gmail.com>
2023-07-08 15:53:06 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-11
 export DT_LINK=https://github.com/RedmiNote10S/recovery_device_redmi_rosemary
-export DT_BRANCH=twrp-11
-export DEVICE_PATH=device/redmj/rosemary
+export DT_BRANCH=android-11
+export DEVICE_PATH=device/redmi/rosemary
 export DEVICE=rosemary
 export MAKEFILE=twrp_rosemary
 export TARGET=bootimage
 export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=13116
+export RANDOM=10041
