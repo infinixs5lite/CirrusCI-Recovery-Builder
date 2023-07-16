f19bbe31ed86d1252431bbc6877559d0503bae76
RecoveryBuilder <ci.twrp@gmail.com>
2023-07-16 06:54:29 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/ShazuxD/extras
-export DT_BRANCH=android-13
+export DT_LINK=https://github.com/shazuxd/extras
+export DT_BRANCH=13
 export DEVICE_PATH=device/xiaomi/sea
 export DEVICE=sea
 export MAKEFILE=twrp_sea
 export TARGET=bootimage
 export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=1913
+export RANDOM=4600
