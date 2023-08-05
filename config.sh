3912c521cfdd0690dcec565004249ff9ab053c67
RecoveryBuilder <ci.twrp@gmail.com>
2023-08-05 17:07:32 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export TWRP_MANIFEST=https://github.com/ShazuxD/platform_manifest_twrp_aosp
-export TWRP_BRANCH=twrp-12.1-1
-export DT_LINK=https://gitlab.com/shazuxd1/d.git
-export DT_BRANCH=h
-export DEVICE_PATH=device/infinix/helios
-export DEVICE=helios
-export MAKEFILE=twrp_helios
+export TWRP_MANIFEST=https://github.com/PitchBlackRecoveryProject/manifest_pb
+export TWRP_BRANCH=android-11.0
+export DT_LINK=https://github.com/ShazuxD/extras
+export DT_BRANCH=android-11-dev-1
+export DEVICE_PATH=device/xiaomi/fleur
+export DEVICE=fleur
+export MAKEFILE=omni_fleur
 export TARGET=bootimage
 export OUTPUT=boot.img
-export ALT_FILE=empty
-export RANDOM=24155
+export ALT_FILE=P*.zip
+export RANDOM=20567
