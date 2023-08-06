f3b12e0ac5276a88fc6a9f816253eb80a263eb42
RecoveryBuilder <ci.twrp@gmail.com>
2023-08-06 11:11:07 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/PitchBlackRecoveryProject/manifest_pb
 export TWRP_BRANCH=android-12.1
-export DT_LINK=https://github.com/infinixs5lite/device_xiaomi_fog-TWRP-PBRP
+export DT_LINK=https://github.com/ShazuxD/device_xiaomi_fog-TWRP-PBRP
 export DT_BRANCH=twrp-12.1
 export DEVICE_PATH=device/xiaomi/fog
 export DEVICE=fog
@@ -9,4 +9,4 @@ export MAKEFILE=omni_fog
 export TARGET=bootimage
 export OUTPUT=boot.img
 export ALT_FILE=P*.zip
-export RANDOM=6190
+export RANDOM=18142
