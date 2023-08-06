97e37a47961e410cd9432e572fe0ce9518e41b20
ShazuxD <shazuxd7@gmail.com>
2023-08-06 15:02:29 +0530

    Notify after build

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 source $CONFIG
+echo -e \
+"
+🛠️ ShazuxD CI
+
+The Build has been Completed!
+Device: "${DEVICE}"
+" > upload.txt
+
+TG_CHAT_ID=-1001983306115
+telegram_message() {
+        curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
+        -d chat_id="${TG_CHAT_ID}" \
+        -d parse_mode="HTML" \
+        -d text="$1"
+}
 cd /tmp/ci/out/target/product/$DEVICE
 echo "Image File:" >> upload.txt
 curl --upload-file $OUTPUT https://free.keep.sh >> upload.txt
@@ -12,11 +27,15 @@ fi
 wget https://github.com/Sushrut1101/GoFile-Upload/raw/master/upload.sh >> /dev/null
 chmod +x upload.sh
 sudo apt install jq -y >> /dev/null
-./upload.sh $OUTPUT
+./upload.sh $OUTPUT >> upload.txt
 echo
 if [ $ALT_FILE = "empty" ] ;then
 echo "No alt file is set"
 else
-./upload.sh $ALT_FILE
+./upload.sh $ALT_FILE >> upload.txt
 fi
 cat upload.txt
+
+p=$(< upload.txt)
+telegram_message "$p"
+echo " "
