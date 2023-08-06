a661a9f35f2cd57a25590d81eee8d675a0fab642
ShazuxD <shazuxd7@gmail.com>
2023-08-06 17:07:10 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ source $CONFIG
 
 cd /tmp/ci
 # Prepare the Build Environment
+git clone https://github.com/dblenk-project/kernel_xiaomi_fog_header --depth=1 kernel/xiaomi/fog
 source build/envsetup.sh
 lunch ${MAKEFILE}-eng
 export ALLOW_MISSING_DEPENDENCIES=true
